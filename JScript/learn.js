5851f610f2a6b98e3cb486b20fe6e35985c92337
Mcd243 <mcd243@protonmail.com>
2022-04-13 21:29:06 +0100

    learn page js coming along

--- JScript/learn.js
+++ JScript/learn.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     // intialize an empty list to store user input
-    const memKey_list = [];
+    let memKey_list = [];
+    const bird = document.getElementById("current").innerHTML;
+    let get_list = localStorage.getItem(bird);
+    memKey_list.push(get_list);
+    console.log(memKey_list)
+
 
     //initialize variables
     
@@ -13,54 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
     function replace(input_id, new_p) {
 
         const old = document.getElementById(input_id);
-        const new = document.createElement('p');
-        new.innerHTML = new_p;
-        old.parentNode.replaceChild(new, old);
+        const new_element = document.createElement('p');
+        new_element.innerHTML = new_p;
+        old.parentNode.replaceChild(new_element, old);
     }
     
     //function to get user input on submitting
     function addToObject(userInput) {
-        userInput.preventDefault();
-        
-        const bird = document.getElementById("current").innerHTML;
-        
-        const key1 = document.getElementById("soundsLike").value;
-        const key2 = document.getElementById("saying").value;
+        userInput.preventDefault();      
+        const bird = document.getElementById("current").innerHTML;   
+        const key1 = document.getElementById("soundsLike").value;    
+        const key2 = document.getElementById("saying").value;    
         const mainKey = document.getElementById("memkey").value;
-
-
-        const object = {
+        
+        let item = {
             name: bird,
             like: key1,
             saying: key2,
             memKey: mainKey
-        };
-        console.log(object)
-
-        
-
-
-
-        
-
-
+        };   
         //insert the user input onto the page
+        replace("soundsLike", key1);
+        replace("saying", key2);
+        replace("memkey", mainKey);
+        //Remove the submit button
+        var submit_button = document.getElementById("submit");
+        submit_button.parentNode.removeChild(submit_button);
 
-
-
-        
-
-        
-       // localStorage.setItem('userListItem', JSON.stringify(myArray));
-      }
-
-    
-
-
-
-
-
-
-
-
+        //Add the object to the list
+        memKey_list.push(item)
+        //send to local storage   
+        localStorage.setItem(bird, JSON.stringify(memKey_list));
+    }
 })
