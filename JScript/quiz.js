358b456ec27f02a830ef04f058bcf6c1760c5a31
Mcd243 <mcd243@protonmail.com>
2022-04-14 21:10:43 +0100

    quiz page js started

---
+++ JScript/quiz.js
@@ -0,0 +1,170 @@
+//Create bird objects with required data
+
+const robin = {
+    name: "Robbin",
+    large_img: "../images/robin_sq.jpg",
+    small_img: "../images/robin.jpg",
+    audio: "../audio/robin.mp3",
+    memkey_hint: "",
+}
+
+const blackbird = {
+    name: "Blackbird",
+    large_img: "../images/blackbird_sq.jpg",
+    small_img: "../images/blackbird.jpg",
+    audio: "../audio/blackbird.mp3",
+    memkey_hint: "",
+}
+
+const chaffinch = {
+    name: "Chaffinch",
+    large_img: "../images/chaffin_sq.jpg",
+    small_img: "../images/chaffin.jpg",
+    audio: "../audio/chaffinch.mp3",
+    memkey_hint: "",
+}
+
+const goldfinch = {
+    name: "Goldfinch",
+    large_img: "../images/goldfinch_sq.jpg",
+    small_img: "../images/goldfinch.jpg",
+    audio: "../audio/goldfinch.mp3",
+    memkey_hint: "",
+}
+
+const great_tit = {
+    name: "Great Tit",
+    large_img: "../images/great_tit_sq.jpg",
+    small_img: "../images/great_tit.jpg",
+    audio: "../audio/great_tit.mp3",
+    memkey_hint: "",
+}
+
+const long_tailed_tit = {
+    name: "Long-tailed Tit",
+    large_img: "../images/long_tailed_tit_sq.jpg",
+    small_img: "../images/long_tailed_tit.jpg",
+    audio: "../audio/long_tailed_tit.mp3",
+    memkey_hint: "",
+}
+
+const magpie = {
+    name: "Magpie",
+    large_img: "../images/magpie_sq.jpg",
+    small_img: "../images/magpie.jpg",  
+    audio: "../audio/magpie.mp3",
+    memkey_hint: "",
+}
+
+const sparrow = {
+    name: "Sparrow",
+    large_img: "../images/sparrow_sq.jpg",
+    small_img: "../images/sparrow.jpg",
+    audio: "../audio/sparrow.mp3",
+    memkey_hint: "",
+}
+
+const starling = {
+    name: "Starling",
+    large_img: "../images/starling_sq.jpg",
+    small_img: "../images/starling.jpg",
+    audio: "../audio/wood_starling.mp3",
+    memkey_hint: "",
+}
+
+const wood_pigeon = {
+    name: "Wood Pigeon",
+    large_img: "../images/wood_pigeon_sq.jpg",
+    small_img: "../images/wood_pigeon.jpg",
+    audio: "../audio/wood_pigeon.mp3",
+    memkey_hint: "",
+}
+
+//Create an array of objects
+const birds = [
+    robin,
+    blackbird,
+    chaffinch,
+    goldfinch,
+    great_tit,
+    long_tailed_tit,
+    magpie,
+    sparrow,
+    starling,
+    wood_pigeon,
+]
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const birds = [
+        robin,
+        blackbird,
+        chaffinch,
+        goldfinch,
+        great_tit,
+        long_tailed_tit,
+        magpie,
+        sparrow,
+        starling,
+        wood_pigeon,
+    ]
+
+    console.log(birds)
+
+    //get a random bird from birds
+        //add audio file to page
+        //get memkey from storage
+        
+
+    //remove bird from list
+
+    // if easy pick 2 birds add to list
+        //add 3 birds to grid 
+        //event listener click
+
+    // if easy pick 5 birds add to list
+        //add 6 birds to grid
+        //event listener click
+
+    // if easy pick 8 birds add to list
+        //add 9 birds to grid
+        //event listener click
+
+    //if get hint button pressed 
+        ////add memkey to page 
+
+    //if chosen bird == selected bird
+        //display popup "correct"
+        //add one to counter
+
+    //else 
+        //display popup "wrong "
+
+    //if page is medium birdlist is empty 
+        //display correct counter out of 10
+        //if correct counter <= 3
+            //keep trying
+        //elif correct counter >3  
+            //very good try hard
+            
+
+    //if page is hard birdlist is empty 
+        //display correct counter out of 10
+        //if correct counter <= 5
+            //keep trying or try medium
+        //elif correct counter >5  
+            //very good keep going
+        //els
+        
+
+
+
+    
+
+
+    
+
+
+
+})    
